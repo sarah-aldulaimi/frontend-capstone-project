3f22f1455bdf1d1365060f9fc07b5dcdfe3098c8
Sarah Al-Dulaimi <Sarah.harithw@gmail.com>
2022-09-29 15:22:13 +0400

    signup component

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { User } from 'src/app/shared/data/user';
+import { Locations } from 'src/app/shared/data/locations';
+import { LocationService } from 'src/app/shared/service/location.service';
+import { UserService } from 'src/app/shared/service/user.service';
 
 @Component({
   selector: 'll-signup',
@@ -6,10 +11,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
+  public locations: Locations[] | undefined;
+  constructor(private userService: UserService, private locationService: LocationService) {}
 
   ngOnInit(): void {
+    this.getLocations();
+  }
+  public registerUser(addForm: NgForm): void {
+    this.userService.addUser(addForm.value).subscribe((res: User) => {
+      console.log(res);
+    });
   }
 
+  public getLocations(): void {
+    this.locationService.getAllLocations().subscribe((res: Locations[]) => {
+      this.locations = res;
+      console.log(this.locations);
+    });
+  }
 }
--- src/app/shared/service/location.service.ts
+++ src/app/shared/service/location.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Locations } from '../data/location';
+import { Locations } from '../data/locations';
 
 @Injectable({
   providedIn: 'root'

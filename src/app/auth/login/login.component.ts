99e465d37c5860a77b84122a9ecc6b6877bdb417
Sarah Al-Dulaimi <Sarah.harithw@gmail.com>
2022-10-12 08:57:56 +0400

    Update login.component.ts

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-// import { CommonModule } from '@angular/common';
 import { UserService } from 'src/app/shared/service/user.service';
 import { User } from 'src/app/shared/data/user';
 import { Role } from 'src/app/shared/data/role';
@@ -20,19 +19,32 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {}
 
   public loginmyForm(addForm: NgForm): void {
-    this.userService.login(addForm.value).subscribe((res: User) => {
+  console.log('1');
+      this.userService.login(addForm.value).subscribe((res: User)=>{
       console.log(res);
-      localStorage.setItem('userId', res.id.toString());
-      if (res != null) {
-        this.checkLoginRole();
-      } else {
-        alert('Login unsuccessful!\nPlease enter the correct email/password!');
-      }
-    });
+       if (res != null) {
+        localStorage.setItem('userId', res.id.toString());
+          }else{
+            alert('Login unsuccessful!\nPlease enter the correct email/password!');
+          }
+        });
+    console.log('2')
+    this.checkLoginRole();
   }
 
   public LoginSuccessful(): void {
     this.router.navigate(['/dashboard']);
+  }
+
+  public checkIfUserExists(addForm: NgForm){
+  this.userService.login(addForm.value).subscribe((res: User)=>{
+  console.log(res);
+   if (res != null) {
+    localStorage.setItem('userId', res.id.toString());
+      }else{
+        alert('Login unsuccessful!\nPlease enter the correct email/password!');
+      }
+    });
   }
 
   public checkLoginRole(): void {
@@ -42,9 +54,10 @@ export class LoginComponent implements OnInit {
       this.userRoles.forEach(element => {
         if (element.name == localStorage.getItem('userRole')) {
           this.LoginSuccessful();
+        }else{
+        alert('Login unsuccessful!\nYou are attempting to sign in as a different role');
         }
       });
-    });
   }
 
   public checkIfUserIsAdmin(): void {
